1bbb173b4af07ae857a22e48a0e31978af1733b0
Meetzli <63364127+meetzli@users.noreply.github.com>
2025-06-29 10:38:04 +0200

    Update plugin for IntelliJ Platform 2025.1 compatibility
    - Fixed syntax highlighter registration issue
    - Added support for RustRover and modern IntelliJ versions
    - Updated build configuration for current Gradle plugin
    - Added ColorSettingsPage for syntax highlighting customization
    - Maintained backward compatibility with existing features
    
    Resolves compatibility issues with IntelliJ Platform 2025.1+

--- src/main/java/com/sercapnp/lang/CapnpLexerAdapter.java
+++ src/main/java/com/sercapnp/lang/CapnpLexerAdapter.java
@@ -5,6 +5,9 @@ import com.intellij.psi.tree.IElementType;
 
 import java.io.Reader;
 
+/**
+ * Created by striky on 6/26/17.
+ */
 public class CapnpLexerAdapter extends FlexAdapter {
     public CapnpLexerAdapter() {
         super(new CapnpLexer((Reader) null));
